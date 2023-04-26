2ab53c2a1e68698752ae3382a91c2acc95fdca6c
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-26 19:25:14 +0100

    develop - added node_group for debug

--- nodes.tf
+++ nodes.tf
@@ -18,9 +18,9 @@ resource "aws_eks_node_group" "main" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
   ]
   lifecycle {
     ignore_changes = [scaling_config[0].desired_size]
