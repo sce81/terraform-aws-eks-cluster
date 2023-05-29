abf422b1094edfa68303cc2caedc10e67af07e78
Simon Elliott <simon.elliott@hashicorp.com>
2023-05-30 00:38:27 +0100

    develop - added support for dynamic variables

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "eks-version" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.main.certificate_authority.0.data
 }
+
+output "nodes_sg_id" {
+  value = aws_security_group.node.id
+}
